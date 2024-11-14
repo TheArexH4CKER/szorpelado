eca587114d958a0906d04c5bc46fab5a8ca91c58
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-11-14 20:11:20 +0100

    Update cumsllr.lua

--- cumsllr.lua
+++ cumsllr.lua
@@ -6,6 +6,8 @@ end
 
 -- CLAIM RANDOM BOOTH
 pcall(function()
+    game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Booths_WipeSavedListings"):FireServer()
+    task.wait(2)
     game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Booths_ClaimAnyBooth"):InvokeServer()
 end)
 
@@ -24,7 +26,7 @@ game:GetService("Players").LocalPlayer.Idled:Connect(function()
     vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
 end)
 
-local price = 51500 -- itt a price nigga
+local price = 52500 -- itt a price nigga
 
 local function sellItem()
     local Buckets = {} 
